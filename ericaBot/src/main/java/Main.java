333f0182a95e25bb6e332bb4128dfc75a906e2b6
kaique-barros <kaiqbarss@gmail.com>
2023-05-20 19:42:26 -0300

    commit-1.1

---
+++ ericaBot/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import net.dv8tion.jda.api.*;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault("MTEwODg1NjQ5MTc5Mzk3MzI0OA.GYkdPc.chIa7cJdQhx66qs36ycQKzDTY9YZl05BSOYqII").build();
+    }
+}
