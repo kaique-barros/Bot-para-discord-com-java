5ccf990cd0effbc40fc4e85ea6bd4e27c5590ce5
kaique-barros <kaiqbarss@gmail.com>
2023-05-21 08:31:59 -0300

    commit-1.3.1

--- ericaBot/src/main/java/Player.java
+++ ericaBot/src/main/java/Player.java
@@ -1,5 +1,27 @@
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Player extends AudioEventAdapter {
+    private final AudioPlayer player;
+    private final BlockingDeque<AudioTrack> queue = new LinkedBlockingDeque<>();
+
+    public Player(AudioPlayer player){
+        this.player = player;
+    }
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        player.startTrack(queue.poll(), false);
+    }
+
+    public void queue(AudioTrack track){
+        if(!player.startTrack(track, true)){
+            queue.offer(track);
+        }
+    }
 
 }
