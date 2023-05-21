7ea82d77a44ae640ca27402965c376a229e922df
kaique-barros <kaiqbarss@gmail.com>
2023-05-20 21:39:07 -0300

    commit-1.1

---
+++ ericaBot/src/main/java/Comandos.java
@@ -0,0 +1,16 @@
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+
+public class Comandos extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        MessageChannel canal = event.getChannel();
+        TextChannel canal =
+
+    }
+}
--- ericaBot/src/main/java/Main.java
+++ ericaBot/src/main/java/Main.java
@@ -1,11 +1,17 @@
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
+
+    public static JDA jda;
     public static void main(String[] args) throws LoginException {
-        String token = "";
-        JDA jda = JDABuilder.createDefault(token).build();
-        jda.addEventListener(new Listeners());
+        String token = "MTEwODg1NjQ5MTc5Mzk3MzI0OA.GfY3TW.BC2E6pCqdMvGc8v2yxChttEwxBNKZmgz7EeRik";
+        jda = JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+
+        jda.addEventListener(new Comandos());
     }
 }
